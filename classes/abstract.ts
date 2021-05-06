5864347e3b4afd594b5487c2b9747f1c38264c08
Minwei Shen <minweishen1991@gmail.com>
2021-05-05 23:42:46 -0700

    add one more example

--- classes/abstract.ts
+++ classes/abstract.ts
@@ -8,3 +8,24 @@ abstract class Base {
     console.log("Hello, " + this.getName());
   }
 }
+
+class Derived extends Base {
+  getName() {
+    return 'name';
+  }
+}
+
+// ===============================================================
+/* Sometimes you want to pass some constructors as parameter*/
+// this is not perfect because you can still call fn(Base) despite the error
+function fn(cons: typeof Base) {
+  const concrete = new cons();
+}
+
+// instead you can use:
+// again, class is a just a syntax sugar in es6
+function fn1(cons: new () => Base) {
+  const concrete = new cons();
+}
+fn1(Base)
+fn1(Derived)
