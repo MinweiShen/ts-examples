5c9da187aa740837a1f92639337c046eb109189d
Minwei Shen <minweishen1991@gmail.com>
2021-06-20 14:47:07 -0700

    some updates

--- examples/order.ts
+++ examples/order.ts
@@ -1,19 +1,25 @@
 #! ./node_modules/.bin/ts-node
 interface OrderStatus {}
-class Success implements OrderStatus {}
-class Failure implements OrderStatus {}
+class Success implements OrderStatus {
+  constructor(public orderPlacedTime: Date) {}
+}
+class Failure implements OrderStatus {
+  constructor(public reason: string) {}
+}
 class Pending implements OrderStatus {}
 
 class Order {}
 
-const statuses = [new Success(), new Failure(), new Pending()];
+const statuses = [new Success(new Date()), new Failure('some reason'), new Pending()];
 const getOrderStatus = (o: Order = new Order()): OrderStatus => statuses[(Math.random() * statuses.length) >> 0]
 
 const status = getOrderStatus();
 
 switch(true) {
   case status instanceof Success:
+    const s: Success = status as Success;
     console.log('success');
+    console.log('order placed at', s.orderPlacedTime)
     break;
   case status instanceof Failure:
     console.log('failure');
@@ -22,7 +28,15 @@ switch(true) {
     console.log('pending');
     break;
   default:
-    console.log('unknown status')
+    console.log('unknown');
 }
+
+const orderHandlers = new Map<OrderStatus, Function>();
+orderHandlers.set(Success, function(s: Success) { console.log('success handler placed at', s.orderPlacedTime);});
+orderHandlers.set(Failure, function() { console.log('failure handler');})
+orderHandlers.set(Pending, function() { console.log('pending handler');})
+
+const handler = orderHandlers.get(status.constructor)
+handler && handler(status)
 
 export {}
